cd373bd0d37052d8161552413e3f71e1a338f5f2
yan zhang <12183975+uiynyny@users.noreply.github.com>
2025-09-22 17:16:22 +0000

    update requirement and database

--- omiver_website/settings.py
+++ omiver_website/settings.py
@@ -100,8 +100,10 @@ WSGI_APPLICATION = 'omiver_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'omiver',
+        'PASSWORD':'password',
+        'NAME': 'omiver_website',
     }
 }
 
