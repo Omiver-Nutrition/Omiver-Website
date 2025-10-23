6e60075086c28fe6015ec88734fe95fe348e1000
githubAdmin <admin@github>
2025-10-23 00:30:18 +0000

    current change

--- api/serializer.py
+++ api/serializer.py
@@ -1,13 +1,38 @@
 from rest_framework import serializers
 
-from core.models import MealPlan, Client
+from core.models import *
+
+
+# Move ProfileSerializer above ClientSerializer to fix NameError
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = [
+            "id",
+            "date_of_birth",
+            "ethnicity",
+            "allergies",
+            "sport",
+            "fitness_goal",
+            "nutritional_goal",
+        ]
+
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = '__all__'
+        fields = [
+            "id",
+            "user",
+            "email",
+            "first_name",
+            "last_name",
+            "type",
+            "profile",
+        ]
+
 
 class MealPlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = MealPlan
-        fields = '__all__'
+        fields = "__all__"
