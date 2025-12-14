93f3a3221527b1e846febda304be4c8406a3a3ae
yan zhang <12183975+uiynyny@users.noreply.github.com>
2025-12-14 10:28:11 +0000

    merge profile with client

--- api/serializer.py
+++ api/serializer.py
@@ -5,31 +5,7 @@ from core.models import *
 from django.contrib.auth.models import User
 
 
-# Move ProfileSerializer above ClientSerializer to fix NameError
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = [
-            "id",
-            "date_of_birth",
-            "sex",
-            "height",
-            "weight",
-            "ethnicity",
-            "allergies",
-            "sport",
-            "health_conditions",
-            "dietary_preferences",
-            "gender",
-            "height",
-            "weight",
-            "fitness_goal",
-            "nutritional_goal",
-        ]
-
-
 class ClientSerializer(serializers.ModelSerializer):
-    profile = ProfileSerializer()
     class Meta:
         model = Client
         fields = [
@@ -39,9 +15,18 @@ class ClientSerializer(serializers.ModelSerializer):
             "first_name",
             "last_name",
             "type",
-            "profile",
+            "date_of_birth",
+            "gender",
+            "height",
+            "weight",
+            "ethnicity",
+            "allergies",
+            "sport",
+            "health_conditions",
+            "dietary_preferences",
+            "fitness_goal",
+            "nutritional_goal",
         ]
-
 
 class MealPlanSerializer(serializers.ModelSerializer):
     class Meta:
